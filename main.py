929c71d67b58b34be1848fa9c0007c1ce6e527b9
fleetster22 <fleetster22@gmail.com>
2020-12-01 20:07:32 -0500

    Day 6 functions

---
+++ main.py
@@ -0,0 +1,10 @@
+# Day 6 of 100
+# Most of it was working on code at https://reeborg.ca/
+# reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Alone&url=worlds%2Ftutorial_en%2Falone.json
+
+# def my_name():
+#     first_name = "Anaka"
+#     last_name = "Norfleet"
+#     print(first_name + " " + last_name)
+#
+# my_name()
